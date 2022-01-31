be34d3077cbd583dd08d65ba6947499c2e8c648e
Bartosz Wieczorek <bartosz.wieczorek1993@gmail.com>
2022-01-31 06:45:48 +0100

    Result

--- 1_ValidPhoneNumber/src/com/company/Main.java
+++ 1_ValidPhoneNumber/src/com/company/Main.java
@@ -9,6 +9,36 @@ Only worry about the following format:
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println(validPhoneNumber("(123) 123-1234"));
+    }
+    public static boolean validPhoneNumber(String phoneNumber) {
+        // TODO: Return whether phoneNumber is in the proper form
+        if(phoneNumber.charAt(0) == '('
+                && phoneNumber.charAt(4)    == ')'
+                && phoneNumber.charAt(5)    == ' '
+                && phoneNumber.charAt(9)    == '-'
+                && phoneNumber.length()     == 14){
+            if(isDigit(phoneNumber.charAt(1))
+                && isDigit(phoneNumber.charAt(2))
+                && isDigit(phoneNumber.charAt(3))
+                && isDigit(phoneNumber.charAt(6))
+                && isDigit(phoneNumber.charAt(7))
+                && isDigit(phoneNumber.charAt(8))
+                    && isDigit(phoneNumber.charAt(10))
+                    && isDigit(phoneNumber.charAt(11))
+                    && isDigit(phoneNumber.charAt(12))
+                    && isDigit(phoneNumber.charAt(13))){
+                return true;
+            }
+        }
+            return false;
+    }
+    public static boolean isDigit(char character){
+        String digits = "0123456789";
+        if(digits.indexOf(character)!=-1){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
