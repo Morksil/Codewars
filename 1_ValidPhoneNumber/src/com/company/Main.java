f192b6f2f69bbca4a94c6f734c4824207b0d1e59
Bartosz Wieczorek <bartosz.wieczorek1993@gmail.com>
2022-01-31 06:49:41 +0100

    Added example solutions

--- 1_ValidPhoneNumber/src/com/company/Main.java
+++ 1_ValidPhoneNumber/src/com/company/Main.java
@@ -1,4 +1,7 @@
 package com.company;
+
+import java.util.regex.Pattern;
+
 /*
 * Write a function that accepts a string, and returns true if it is in the form of a phone number.
 Assume that any integer from 0-9 in any of the spots will produce a valid phone number.
@@ -42,3 +45,45 @@ public class Main {
         }
     }
 }
+//Example solutions
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+import java.util.regex.Pattern;
+public class Kata {
+  public static boolean validPhoneNumber(String phoneNumber) {
+    return Pattern.matches("^\\(\\d{3}\\) \\d{3}-\\d{4}$", phoneNumber);
+  }
+}
+*/
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+import java.util.regex.*;
+
+public class Kata {
+    public static boolean validPhoneNumber(String phoneNumber) {
+        // TODO: Return whether phoneNumber is in the proper form
+    */
+/*
+    \\( --- Start with an "("
+    (\\d{3}) --- Followed by 3 digits
+    \\) --- Have an ")"
+    (\\s) --- Have an space
+    (\\d{3}) --- Followed by 3 digits.
+    (\\-) --- Have a "-" after numeric digits
+    (\\d{4}) - Ends with four digits
+    *//*
+
+        Pattern p = Pattern.compile("\\((\\d{3})\\)(\\s)(\\d{3})(\\-)(\\d{4})");
+        if (p.matcher(phoneNumber).matches()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}*/
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*public class Kata {
+    public static boolean validPhoneNumber(String phoneNumber) {
+        return phoneNumber != null && phoneNumber.matches("\\([0-9]{3}\\)\\s[0-9]{3}\\-[0-9]{4}");
+    }
+}*/
