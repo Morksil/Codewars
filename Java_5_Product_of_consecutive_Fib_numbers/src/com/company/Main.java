cb9dc86b14c4ba80244365d32e507a3d91c44606
Bartosz Wieczorek <bartosz.wieczorek1993@gmail.com>
2022-02-04 05:35:18 +0100

    First commit

--- Java_5_Product_of_consecutive_Fib_numbers/src/com/company/Main.java
+++ Java_5_Product_of_consecutive_Fib_numbers/src/com/company/Main.java
@@ -1,8 +1,31 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(Arrays.toString(productFib(4895L)));
+    }
+    public static long[] productFib(long prod) {
+        List<Long> listFibonacci = new ArrayList<>();
+        listFibonacci.add(0L);
+        listFibonacci.add(1L);
+        boolean isOK = false;
+        int i = 2;
+        while(true){
+            if(listFibonacci.get(i-1)*listFibonacci.get(i-2) > prod){
+                isOK = false;
+                return new long[] {listFibonacci.get(i-2), listFibonacci.get(i-1), 0};
+            }else if(listFibonacci.get(i-1)*listFibonacci.get(i-2) == prod){
+                isOK = true;
+                return new long[] {listFibonacci.get(i-2), listFibonacci.get(i-1), 1};
+            }
+            listFibonacci.add(listFibonacci.get(i-2) + listFibonacci.get(i-1));
+            i++;
+        }
     }
 }
+
