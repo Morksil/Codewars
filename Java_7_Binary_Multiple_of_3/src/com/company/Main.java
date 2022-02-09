d8c1ca37860dc076881b0cbc2000d960804eac23
Bartosz Wieczorek <bartosz.wieczorek1993@gmail.com>
2022-02-09 05:44:05 +0100

    Solution

--- Java_7_Binary_Multiple_of_3/src/com/company/Main.java
+++ Java_7_Binary_Multiple_of_3/src/com/company/Main.java
@@ -1,8 +1,14 @@
 package com.company;
 
+import java.util.regex.Pattern;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        boolean b2 = multipleOf3().matcher("000").matches();
+        System.out.println(b2);
+    }
+    public static Pattern multipleOf3() {
+        return Pattern.compile("(1(01*0)*1|0)*");
     }
 }
